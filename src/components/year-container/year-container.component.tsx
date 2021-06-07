bb4a169a3c3c3aeda21cc53da98db1ccef5cff41
Brady Peterson <bpeterson1663@gmail.com>
2021-06-06 19:57:16 -0500

    style(prettier): ran prettier

--- src/components/year-container/year-container.component.tsx
+++ src/components/year-container/year-container.component.tsx
@@ -3,13 +3,15 @@ import { Year, Square } from './year-container.styles'
 import { SquareT } from '../../types/types.constants'
 const squares: SquareT[] = []
 
-for(let i = 0; i < 366; i++){
-    squares.push({id: i, date: new Date().toISOString(), isComplete: false, logId: 'logId' })
+for (let i = 0; i < 366; i++) {
+  squares.push({ id: i, date: new Date().toISOString(), isComplete: false, logId: 'logId' })
 }
 const YearContainer = () => (
-    <Year>
-        {squares.map(() => <Square />)}
-    </Year>
+  <Year>
+    {squares.map(() => (
+      <Square />
+    ))}
+  </Year>
 )
 
 export default YearContainer
